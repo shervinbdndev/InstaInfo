f2407ba3f02625c7cc257f0cf38d622e88728410
Shervin <68682979+shervin-glitch@users.noreply.github.com>
2021-07-25 15:42:14 -0700

    Update Instainfo.py

--- InstaInfo/Instainfo.py
+++ InstaInfo/Instainfo.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
         import colorama
         import cfonts
         import time
+        import json
+        import socket
     except:
         from requests import get
         from instagramy import InstagramUser
@@ -12,6 +14,8 @@ if __name__ == "__main__":
         from colorama.ansi import Fore
         from cfonts import render , cli
         from time import sleep
+        from json import loads
+        from socket import gethostbyname , gethostname
 
     C = [
         f"{colorama.ansi.Fore.WHITE}",
@@ -21,6 +25,11 @@ if __name__ == "__main__":
         f"{colorama.ansi.Fore.YELLOW}",
         f"{colorama.ansi.Fore.RED}"
     ]
+
+    L_IP = socket.gethostbyname(socket.gethostname())
+    RE_Q = requests.get("https://api.myip.com").content
+    LOAD = json.loads(RE_Q)
+    P_IP = LOAD["ip"]
 
     H = """
              .:--================--:.             
@@ -50,13 +59,18 @@ if __name__ == "__main__":
 
     def MainScript():
         time.sleep(1.5)
-        INPUT = str(input(f"{C[1]}[{C[3]}~{C[1]}] {C[1]}({C[5]}U$ERNAME{C[1]}){C[2]} --@{C[0]} "))
+        print(f"\n\n{C[1]}[{C[3]}Local IP{C[1]}] {C[0]}: {C[2]}{L_IP}")
+        print(f"\n{C[1]}[{C[3]}Public IP{C[1]}] {C[0]}: {C[2]}{P_IP}")
+        INPUT = str(input(f"\n\n{C[1]}[{C[3]}~{C[1]}] {C[1]}({C[5]}U$ERNAME{C[1]}){C[2]} --@{C[0]} "))
         i = instagramy.InstagramUser(INPUT)
-        print(f"{C[1]}[{C[3]}Verified{C[1]}] {C[0]}: {C[2]}{i.is_verified}")
-        print(f"{C[1]}[{C[3]}Username{C[1]}] {C[0]}: {C[2]}{INPUT}")
+        print(f"\n{C[1]}[{C[3]}Verified{C[1]}] {C[0]}: {C[2]}{i.is_verified}")
+        print(f"{C[1]}[{C[3]}Private{C[1]}] {C[0]}: {C[2]}{i.is_private}")
+        print(f"{C[1]}[{C[3]}Username{C[1]}] {C[0]}: {C[2]}@{INPUT}")
         print(f"{C[1]}[{C[3]}Full name{C[1]}] {C[0]}: {C[2]}{i.fullname}")
         print(f"{C[1]}[{C[3]}Followers{C[1]}] {C[0]}: {C[2]}{i.number_of_followers}")
         print(f"{C[1]}[{C[3]}Followings{C[1]}] {C[0]}: {C[2]}{i.number_of_followings}")
         print(f"{C[1]}[{C[3]}Posts{C[1]}] {C[0]}: {C[2]}{i.number_of_posts}")
         print(f"{C[1]}[{C[3]}Website{C[1]}] {C[0]}: {C[2]}{i.website}")
+        print(f"{C[1]}[{C[3]}Biography{C[1]}] {C[0]}: {C[2]}{i.biography}")
+        print(f"{C[1]}[{C[3]}Profile Picture Urls{C[1]}] {C[0]}: {C[2]}{i.profile_picture_url}")
     MainScript()
