4e71b51a75506f07f55afa85826be55181c3f989
Shervin <68682979+shervin-glitch@users.noreply.github.com>
2021-07-25 15:42:39 -0700

    Update requirements.py

--- InstaInfo/requirements.py
+++ InstaInfo/requirements.py
@@ -20,6 +20,10 @@ if __name__ == "__main__":
                 import cfonts
             except:
                 print(r"[-] cfonts package is not Installed")
+            try:
+                import socket
+            except:
+                print(r"[-] socket package is not Installed")
 
     PackageManager()
 
@@ -29,6 +33,7 @@ if __name__ == "__main__":
                 INPUT = str(input("[?] Do you want To Download and install Requirements ? (y/n) : "))
                 if str(INPUT) == "y" or str(INPUT) == "Y" or str(INPUT) == "yes" or str(INPUT) == "Yes":
                     subprocess.call(["pip3" , "install" , "requests"]) and subprocess.call(["pip" , "install" , "requests"])
+                    subprocess.call(["pip3" , "install" , "sockets"]) and subprocess.call(["pip" , "install" , "sockets"])
                     subprocess.call(["pip3" , "install" , "colorama"]) and subprocess.call(["pip" , "install" , "colorama"])
                     subprocess.call(["pip3" , "install" , "instagramy"]) and subprocess.call(["pip" , "install" , "instagramy"])
                     subprocess.call(["pip3" , "install" , "python-cfonts"]) and subprocess.call(["pip" , "install" , "python-cfonts"])
